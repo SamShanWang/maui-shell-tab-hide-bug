94dbaf2e1b6d57dee85d6e9c592a991c0b322e28
ws <wangshan@outlook.com>
2022-07-19 21:57:31 +0800

    update comment.

--- MauiAppShellTabHideBug/AppShell.xaml.cs
+++ MauiAppShellTabHideBug/AppShell.xaml.cs
@@ -9,13 +9,13 @@ public partial class AppShell : Shell
 
     private void MenuItem_Clicked(object sender, EventArgs e)
     {
-        // It's OK.
+        // bug: It's OK in Android platfore, but has bug in Windows platform.
         //Tab1.IsVisible = false;
 
         Tab2.IsVisible = true;
         Tab3.IsVisible = true;
 
-        // bug
+        // bug: It's OK in Windows platfore, but has bug in Android platform.
         Tab1.IsVisible = false;
     }
 }
